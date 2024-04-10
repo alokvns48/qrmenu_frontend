9fc4c957aeee46dd3dc305da0c50f91729ecae7c
alokvns48 <alokvns48@gmail.com>
2024-04-10 21:29:17 +0530

    init

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
+import MainLayout from "../layouts/MainLayout";
 
-const Login = () => <div>Login</div>;
+const Login = () => <MainLayout>Login</MainLayout>;
 
 export default Login;
